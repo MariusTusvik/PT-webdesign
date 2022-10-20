989a735658195377cb2848fba0a3b39af35c05f0
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2022-10-20 13:34:03 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App name ='min app' />
+    <Footer/>
   </React.StrictMode>
 );
