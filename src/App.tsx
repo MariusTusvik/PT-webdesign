39cf80339a5496b67b92146febc4f1b205351fa8
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-10-22 15:17:34 +0200

    WAH

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { Outlet, Link } from "react-router-dom";
 import Booking from './Pages/Booking';
 import Shop from './Pages/Shop';
+import logowhite from './assets/logowhite.png';
 
 function App() {
   return (
-   <div>
-  <nav className=' border-b-4 border-black pb-4'>
-  <Link to='/booking'>Booking</Link> |{" "}
-  <Link to='/shop'>Shop</Link>
-  <Outlet />
-
-  </nav>    
+   <div className='bg-[#1e1e1e] items-center flex'>
+    <img src={logowhite} className="flex "/>
    </div>
   );
 }
