b74e0d7f7d829575281ed1bb67e0af9aba225f9d
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-10-22 19:59:55 +0200

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,22 @@ import logowhite from './assets/logowhite.png';
 
 function App() {
   return (
-    <div className='items-center align-middle'>
-   <div className='bg-[#1e1e1e] items-center flex align-middle'>
-    <img src={logowhite} className=""/>
-   </div>
-    <div className='bg-[#1e1e1e] items-center text-white font-bold'>
-      <p>
+  <div>
+    <div>
+
+    </div>
+
+    <div className='bg-[#1e1e1e] place-content-center flex py-10'>
+      <img src={logowhite} className=""/>
+    </div>
+    
+    <div className='bg-[#1e1e1e] text-white font-bold w-full place-content-center pb-20'>
+      <p className=' text-center items-center'>
         we patiently await the inevitable waking of azathoth the blind idiot god
       </p>
     </div>
-    </div>
+  
+  </div>
   );
 }
 
