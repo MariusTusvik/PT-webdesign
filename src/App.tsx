958ee6da16a7545f0f1a1cb0f99dac349adbe120
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-10-22 17:59:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import logowhite from './assets/logowhite.png';
 
 function App() {
   return (
-   <div className='bg-[#1e1e1e] items-center flex'>
-    <img src={logowhite} className="flex "/>
+    <div className='items-center align-middle'>
+   <div className='bg-[#1e1e1e] items-center flex align-middle'>
+    <img src={logowhite} className=""/>
    </div>
+    <div className='bg-[#1e1e1e] items-center text-white font-bold'>
+      <p>
+        we patiently await the inevitable waking of azathoth the blind idiot god
+      </p>
+    </div>
+    </div>
   );
 }
 
