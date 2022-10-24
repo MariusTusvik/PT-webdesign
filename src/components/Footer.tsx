0f5620a8ed3ea7bb65d587e360f9f3fda7e0eaf4
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2022-10-24 20:06:15 +0200

    imported all png's

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Facebook from '../assets/Facebook.png'
-
+import Twitter from '../assets/Twitter.png'
+import Tiktok from '../assets/Tiktok.png'
+import Instagram from '../assets/Instagram.png'
 
 function Footer () {
     return (
         <footer className='bg-red'>
-        <div className='flex'>
-
-        <img src={Facebook} className="object-scale-down h-20 w-30 items-center p-1" />
-
-        </div>
+            <div className='flex'>
+                <img src={Facebook} className="object-scale-down h-20 w-30 items-center p-1" />
+                <img src={Twitter} className="object-scale-down h-20 w-30 items-center p-1" />
+                <img src={Tiktok} className="object-scale-down h-20 w-30 items-center p-1" />
+                <img src={Instagram} className="object-scale-down h-20 w-30 items-center p-1" />
+            </div>
         </footer>
     );
 }
