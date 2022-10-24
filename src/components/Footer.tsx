6a415a11ddcfefba8a591c2dde10b13a97446113
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2022-10-24 20:43:51 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,19 @@ import Instagram from '../assets/Instagram.png'
 function Footer () {
     return (
         <footer className='bg-red'>
-            <div className='flex'>
+            <div className='row flex bg-black'>
                 <img src={Facebook} className="object-scale-down h-20 w-30 items-center p-1" />
                 <img src={Twitter} className="object-scale-down h-20 w-30 items-center p-1" />
                 <img src={Tiktok} className="object-scale-down h-20 w-30 items-center p-1" />
                 <img src={Instagram} className="object-scale-down h-20 w-30 items-center p-1" />
             </div>
+
+            <div className='row flex'>
+                <div className='col-xs-10 col-sm-8 bg-red-600'>
+                    "DJ Chicken Has Been Here"
+                </div> 
+            </div>  
+
         </footer>
     );
 }
