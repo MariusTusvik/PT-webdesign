bd765b021a505a8674441b60f2189926447890a1
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-10-24 11:14:27 +0200

    bing bong

--- src/Pages/OmOss.tsx
+++ src/Pages/OmOss.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import gym from '../assets/Gym.jpg'
+import gym from '../assets/Gym.png'
 
 
 function OmOss() {
   return (
-<div className='flex'>
-    <img src={gym} className='flex h-30 w-30'/>
-    <h5 className='flex'>
-      sdfgsdfg
-    </h5>
+    <div className='flex'>
+      <img src={gym} className='flex h-30 w-30' />
+      <h5 className='flex'>
+        sdfgsdfg
+      </h5>
 
-</div>
+    </div>
 
 
   );
