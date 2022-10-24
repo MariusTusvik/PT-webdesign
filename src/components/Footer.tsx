fc963e0a1880c2f57c5f7d35b254a0f2ab4c0dd4
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2022-10-24 19:55:55 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Facebook from '../assets/Facebook.png'
 
 
 function Footer () {
     return (
         <footer className='bg-red'>
         <div className='flex'>
-        
 
+        <img src={Facebook} className="object-scale-down h-20 w-30 items-center p-1" />
 
         </div>
         </footer>
