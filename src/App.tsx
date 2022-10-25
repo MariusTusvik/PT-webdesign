10f52aa0984fe2141726aa9f7ede4865a6fc1b24
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-10-25 14:35:01 +0200

    Om oss and lines

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import logoandstar from './assets/Logoandstar.png'
 
 function App() {
   return (
-  <div>
-    <div className='bg-[#1e1e1e] place-content-center flex py-10'>
-      <img src={logoandstar} className="flex"/>
+    <div className='bg-[#1A1A1A]'>
+      <hr className='line py-10' />
+      <div className='place-content-center flex py-10'>
+        <img src={logoandstar} className="flex" />
+      </div>
+
+      <div className='text-white font-bold w-full place-content-center pb-20'>
+        <p className=' text-center items-center text-2xl'>
+          we patiently await the inevitable waking of azathoth the blind idiot god
+        </p>
+      </div>
+
     </div>
-    
-    <div className='bg-[#1e1e1e] text-white font-bold w-full place-content-center pb-20'>
-      <p className=' text-center items-center text-2xl'>
-        we patiently await the inevitable waking of azathoth the blind idiot god
-      </p>
-    </div>
-  
-  </div>
   );
 }
 
 
 export default App;
- 
--- src/Pages/OmOss.tsx
+++ src/Pages/OmOss.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import gym from '../assets/Gym.png';
-import '../index.css';
 
 
 function OmOss() {
   return (
-    <body className='bg-[#212121]'>
-      <div>
-        sadfgsdaf
-      </div>
+    <body className='bg-[#212121] px-56 py-20'>
 
-      <div className='line' > </div>
+      <hr className='line py-10' />
 
-      <div className='flex p-32 gap-20 pb-10'>
+      <div className='flex  gap-20 '>
         <img src={gym} className='flex imgOm' />
 
-        <h5 className='flex text-[#12a8f4] font-bold text-5xl'>
+        <h5 className='flex text-[#12a8f4] font-bold text-7xl w-3/4 py-20'>
           Cthulu is real and you should come pray with us
         </h5>
       </div>
-      <div className='pl-32 text-[#F746A1] text-3xl font-bold '>
-        read more about simens bad titles here
+
+      <div className='text-[#F746A1] text-4xl font-bold items-end'>
+        <h4 className='p-10'>
+          Read more about simens bad titles here
+        </h4>
+        <hr className='pinkline' />
       </div>
+
+      <div className='text-white  text-ellipsis w-1/2 p-10'>
+        <p className=' text-2xl break-normal'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus est pellentesque elit ullamcorper dignissim. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra.
+        </p>
+      </div>
+
     </body>
 
 
