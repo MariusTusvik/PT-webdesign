364417af7edcd7648bbab3d40a7f98e14f64cacd
MariusTusvik <115139566+MariusTusvik@users.noreply.github.com>
2022-11-17 10:07:27 +0100

    login authenticato

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -2,19 +2,10 @@ import React, { HtmlHTMLAttributes, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import logo from '../assets/logo.png';
 import { Outlet, Link } from "react-router-dom";
-import { signInWithPopup, GoogleAuthProvider} from "firebase/auth"
-import { auth } from '../lib/firebase';
 
 
 function LogIn() {
     const [name, setName] = useState("");
-
-    const login = async () => {
-        
-        const credentials = await signInWithPopup(auth, new GoogleAuthProvider())
-        console.log(credentials.user)
-    }
-
     return (
 
         <body className="bg-[#1a1a1a] justify-center items-center flex-col flex">
@@ -23,7 +14,7 @@ function LogIn() {
             <div className="  bg-[#242424] w-96 bg-center justify-center items-center flex-col flex p-10">
                 <img src={logo} className="object-scale-down h-20 w-30 items-center p-1" />
 
-                <form className='items-center justify-center flex flex-col' onSubmit={e => e.preventDefault()}>
+                <form className='items-center justify-center flex flex-col' action="">
                     <label>
                         <p className=' text-3xl text-center text-white font-bold pb-16 items-center'> Log in. </p>
                         
@@ -33,7 +24,6 @@ function LogIn() {
                             name='groupname'
                             value={name}
                             onChange={(e) => setName(e.target.value)}
-                            
                         />
                    
                     </label>
@@ -41,7 +31,7 @@ function LogIn() {
                     <Link to='/' className=' text-[#12A8F4] pb-16 h-full p-5 text-left'> Har du glemt e-postaddressen ?</Link>
                     <Link to='/' className=' text-[#F746A1] pb-16 h-full p-5'> Har du ikke en bruker?</Link>
 
-                    <button type="button" onClick={login}>Submit</button>
+                    
 
                 </form>
             </div>
